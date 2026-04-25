16878252708375303884b9789781d10def8155e3
Joshua Cline <wwjdtd@gmail.com>
2026-04-25 12:50:04 -0600

    Create 2-no-back-at-home.lua

---
+++ 2-no-back-at-home.lua
@@ -0,0 +1,30 @@
+--[[
+    This user patch disables the back button when you are in the home folder.
+--]]
+
+local FileChooser = require("ui/widget/filechooser")
+
+
+function FileChooser:onBack()
+    local back_to_exit = G_reader_settings:readSetting("back_to_exit", "prompt")
+    local back_in_filemanager = G_reader_settings:readSetting("back_in_filemanager", "default")
+    if back_in_filemanager == "default" then
+        if back_to_exit == "always" then
+            return self:onClose()
+        elseif back_to_exit == "disable" then
+            return true
+        elseif back_to_exit == "prompt" then
+            UIManager:show(ConfirmBox:new{
+                text = _("Exit KOReader?"),
+                ok_text = _("Exit"),
+                ok_callback = function()
+                    self:onClose()
+                end,
+            })
+            return true
+        end
+    elseif back_in_filemanager == "parent_folder" then
+        self:onFolderUp()
+        return true
+    end
+end
